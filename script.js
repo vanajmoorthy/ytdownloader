634fa9c4f5e10b6565524d3e5b972a5f6c021619
vanajmoorthy <vanajmoorthy@gmail.com>
2020-01-13 20:01:17 +0530

    pls work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var mp3Btn = document.getElementById('mp3');
 var	mp4Btn = document.getElementById('mp4');
 var URLinput = document.querySelector('.URL-input');
-var server = 'http://localhost:4000';
+// var server = 'http://localhost:4000';
 
 mp3Btn.addEventListener('click', () => {
 	console.log(`URL: ${URLinput.value}`);	
@@ -15,9 +15,9 @@ mp4Btn.addEventListener('click', () => {
 });
 
 function redirectMp3(query) {
-	window.location.href = `${server}/downloadmp3?url=${query}`;
+	window.location.href = `/downloadmp3?url=${query}`;
 }
 
 function redirectMp4(query) {
-	window.location.href = `${server}/downloadmp4?url=${query}`;
+	window.location.href = `/downloadmp4?url=${query}`;
 }
